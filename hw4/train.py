07f8d5808044cb9c61b96019970a902921319931
SheekhaJ <jariwals@oregonstate.edu>
2019-05-18 23:53:14 -0700

    Files for hw4

---
+++ hw4/train.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import sys
+import time
+from svector import svector
+
+def read_from(textfile):
+    for line in open(textfile):
+        label, words = line.strip().split("\t")
+        yield (1 if label=="+" else -1, words.split())
+
+def make_vector(words):
+    v = svector()
+    for word in words:
+        v[word] += 1
+    return v
+    
+def test(devfile, model):
+    tot, err = 0, 0
+    for i, (label, words) in enumerate(read_from(devfile), 1): # note 1...|D|
+        err += label * (model.dot(make_vector(words))) <= 0
+    return err/i  # i is |D| now
+            
+def train(trainfile, devfile, epochs=5):
+    t = time.time()
+    best_err = 1.
+    model = svector()
+    for it in range(1, epochs+1):
+        updates = 0
+        for i, (label, words) in enumerate(read_from(trainfile), 1): # label is +1 or -1
+            sent = make_vector(words)
+            if label * (model.dot(sent)) <= 0:
+                updates += 1
+                model += label * sent
+        dev_err = test(devfile, model)
+        best_err = min(best_err, dev_err)
+        print("epoch %d, update %.1f%%, dev %.1f%%" % (it, updates / i * 100, dev_err * 100))
+    print("best dev err %.1f%%, |w|=%d, time: %.1f secs" % (best_err * 100, len(model), time.time() - t))
+
+if __name__ == "__main__":
+    train(sys.argv[1], sys.argv[2], 10)
